a289173bfb4599fe11d77c60bf558ba4ba21be94
smndf <simon.dif.sd@gmail.com>
2014-12-14 23:19:50 +0100

    Dinic a l'air ok

--- src/algorithmen/Dinic.java
+++ src/algorithmen/Dinic.java
@@ -21,6 +21,7 @@ public class Dinic {
 	public void dinic(Graph graph, int start, int target){
 		int n = graph.getKnotenPosition().size();
 		int[][] flow = new int[n][n];
+		int totalFlow = 0;
 		ArrayList<Integer> paths = new ArrayList<Integer>();
 
 		int flowMax = 0;
@@ -41,126 +42,128 @@ public class Dinic {
 		residualGraph.setKnotenPosition(graph.getKnotenPosition());
 
 		// graphe de niveau
-		Graph acyclicSubgraph = getAcyclicSubgraph(residualGraph, start, target);
+		Graph acyclicSubgraph;
 
-		while (BFS(acyclicSubgraph.getCapacity(),start,target) != null){
-			// on ajoute les nouveaux chemins
-			paths.addAll(BlockingFlow(acyclicSubgraph.getCapacity(), start, target));
-			acyclicSubgraph = getAcyclicSubgraph(residualGraph, start, target);
+		while (BFS((acyclicSubgraph = getAcyclicSubgraph(residualGraph, start, target)).getCapacity(),start,target) != null){
+			System.out.println("nouveau acyclic graph :");
+			acyclicSubgraph.printGraph();
+
+			int[] path;
+			while ((path = BFS(acyclicSubgraph.getCapacity(),start,target)) != null){
+				acyclicSubgraph.printGraph();
+
+				// on calcule la longueur du path
+				int u = target;
+				int pathLength = 0;
+				while (path[u]!=-1){
+					pathLength++;
+					u = path[u];
+				}
+
+				// on trouve le flux max du path
+				int minCapacityPath = Integer.MAX_VALUE;
+				int capacity = 0;
+				u = target;
+				for (int i = 0; i < pathLength; i++){
+					System.out.println("u : " + u + "    path[u] : " + path[u]);
+					capacity = acyclicSubgraph.getCapacity()[path[u]][u];
+					u = path[u];
+					if (capacity < minCapacityPath){
+						minCapacityPath = capacity;
+					}
+				}
+				System.out.println("flux :" + minCapacityPath);
+				totalFlow += minCapacityPath;
+				
+				// on met à jour les graphes
+				u = target;
+				for (int i = 0; i < pathLength; i++){
+					residualGraph.getCapacity()[path[u]][u] -= minCapacityPath;
+					residualGraph.getCapacity()[u][path[u]] += minCapacityPath;
+					acyclicSubgraph.getCapacity()[path[u]][u] -= minCapacityPath;
+					acyclicSubgraph.getCapacity()[u][path[u]] += minCapacityPath;
+					u = path[u];
+				}
+
+
+			}
+
 		}
+		System.out.println("flux total :" + totalFlow);
 	}
 
 	// graphe de niveau (= graphe de départ sans les arcs qui créent des chemins plus longs que ceux existant)
-	// on retire les valeurs des chemins trouvés pour en trouver d'autres avec bfs, 
-	// surement pas comme ça qu'il faut faire
-	// il faudrait créer une nouvelle BFS qui renvoie pathlist contenant tous les chemins de longueur minimale
-	// => BFSDinic
 	private Graph getAcyclicSubgraph(Graph graph, int start, int target) {
 		int n = graph.getKnotenPosition().size();
-		ArrayList<int[]> pathlist = new ArrayList<int[]>();
 		int[][] acyclicGraphCapacity = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				acyclicGraphCapacity[i][j] = 0;
 			}
 		}
-		Graph acyclicGraph = new Graph(graph.getKnotenPosition(),acyclicGraphCapacity);
-		System.out.println("acyclicGraph :");
-		acyclicGraph.printGraph();
 
-		pathlist = DFSDinic(graph.getCapacity(), start, target);
-		/*int[] path = BFS(graph.getCapacity(), start, target);
-
-		while (path != null){
-			pathlist.add(path);
-
-			// MAJ graph avant de relancer bfs
-			int capacityPath = getCapacityPath(graph, path, target, start);
-			int v = target;
-			int u;
-			while (v != start) {
-				u = path[v];
-				System.out.println("flow : " + u + " , " + v + " : " + capacityPath );
-				graph.getCapacity()[u][v] -= capacityPath;
-				graph.getCapacity()[v][u] += capacityPath;
-				acyclicGraph.getCapacity()[u][v] += capacityPath;
-				v = u;
+		Graph acyclicGraph = new Graph();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				acyclicGraphCapacity[i][j] = graph.getCapacity()[i][j];
 			}
-			System.out.println("acyclicGraph :");
-			acyclicGraph.printGraph();
+		}
+		acyclicGraph.setCapacity(acyclicGraphCapacity);
+		acyclicGraph.setKnotenPosition(graph.getKnotenPosition());
 
-			path = BFS(graph.getCapacity(), start, target);
-		}*/
-		return acyclicGraph;
-	}
-
-
-	public ArrayList<int[]> DFSDinic(int[][] graph, int start, int target) {
-
-		ArrayList<int[]> pathlist = new ArrayList<int[]>();
-		int n = graph.length;
-		//int[] isSeen = new int[n];
+		int[] isSeen = new int[n];
 		int[] d = new int[n];
 		int[] path = new int[n];
-		int bestDistance = Integer.MAX_VALUE;
-		//boolean exist = false;
 		int u;
-		//LinkedList<Integer> queue = new LinkedList<Integer>();
-		Stack<Integer> stack = new Stack<Integer>();
+		LinkedList<Integer> queue = new LinkedList<Integer>();
 		for (int i = 0; i < n; i++){
-			//isSeen[i] = 0;
+			isSeen[i] = 0;
 			d[i] = Integer.MAX_VALUE;
 			path[i] = -1; 
 		}
-		//isSeen[start] = 1;
+		isSeen[start] = 1;
 		d[start] = 0;
 
-		stack.push(start);
-		System.out.println("uktifuolyjk");
-		while (!stack.isEmpty()){
-			u = stack.pop();
-			for (int v : getAdjacent(graph, u)){ 
-				System.out.println("bestDistance = " + bestDistance);
-				if (d[v] > d[u] + 1){
-					System.out.println("gfc");
+		queue.add(start);
+		while (!queue.isEmpty()){
+			System.out.println("file : " + queue.toString());
+			u = queue.poll();
+			for (int v : getAdjacent(graph.getCapacity(), u)){ 
+				if (d[v] >= d[u] + 1){
 					d[v] = d[u] + 1;
 					path[v] = u;
-					if (v == target){
-						//exist = true;
-						System.out.println("d[v] + " + d[v] + " , bestdistance :" + bestDistance);
-						if (d[v] == bestDistance){
-							pathlist.add(path);
-							int t = target;
-							System.out.print("path : " + target + " , ");
-							while (t!=start){
-								System.out.print( path[t] + " , ");
-								t = path[t];
+					/*if (v == target){
+							if (d[v] == bestDistance){
+								pathlist.add(path);
+								int t = target;
+								while (t!=start){
+									t = path[t];
+								}
 							}
-							System.out.println("");
-						}
-						else if (d[v] < bestDistance){
-							pathlist.clear();
-							pathlist.add(path);
-							int t = target;
-							System.out.print("path : " + target + " , ");
-							while (t!=start){
-								System.out.print( path[t] + " , ");
-								t = path[t];
+							else if (d[v] < bestDistance){
+								pathlist.clear();
+								pathlist.add(path);
+								int t = target;
+								while (t!=start){
+									t = path[t];
+								}
+								bestDistance = d[v];
 							}
-							System.out.println("");
-							bestDistance = d[v];
-						}
-						
-						// quand on a trouvé un chemin, on vide la pile et on recommence une nouvelle recherche
-						stack.clear();
-						stack.push(start);
-					} else {
-						stack.push(v);
-					}
-				}	
+						} else {
+							queue.add(v);
+						}*/
+					queue.add(v);
+				} else {
+					// on enleve l'arc
+					acyclicGraphCapacity[u][v] = 0;
+					System.out.println("arc "+u+","+v+" enlevé");
+				}
 			}
 		}
-		return pathlist;
+		System.out.println("fin getAcyclicSubgraph");
+		return acyclicGraph;
+
+
 	}
 
 	public int[] BFS(int[][] graph, int start, int target) {
@@ -201,12 +204,12 @@ public class Dinic {
 
 		if (exist){
 			u = target;
-			//System.out.print("path : " + target + " , ");
+			System.out.print("path : " + target + " , ");
 			while (u!=start){
-				//System.out.print( path[u] + " , ");
+				System.out.print( path[u] + " , ");
 				u = path[u];
 			}
-			//System.out.println("");
+			System.out.println("");
 			return path;			
 		}else {
 			return null;
